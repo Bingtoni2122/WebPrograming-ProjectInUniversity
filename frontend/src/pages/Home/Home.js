0e5523e1e7d6b183a1cfa5f75cd690a321956978
_bing2122 <97716130+Bingtoni2122@users.noreply.github.com>
2023-07-10 12:34:01 +0700

    Code Files

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import style from "../../style";
+import Hero from "../Hero/Hero.js";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import React from "react";
+
+const Home = () => {
+    
+    
+    return (
+        <>
+            <Hero />
+            
+        </>
+    );
+};
+export default Home;
